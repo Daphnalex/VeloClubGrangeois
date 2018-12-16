b585bb069457c03a24f1422be2de40c5a594788b
Alexandre Cuillerier <ac@pierlis.com>
2018-12-16 22:25:58 +0100

    Config creditor

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,5 @@ module VcgRuby
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w( ckeditor/* )
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += Ckeditor.assets
 Rails.application.config.assets.precompile += %w( ckeditor/*)
+Rails.application.config.autoload_paths += %W(#{Rails.application.config.root}/app/models/ckeditor)
+
