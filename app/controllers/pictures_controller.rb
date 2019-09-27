d0033914c7fc28d1723b2232f5d644c3f409fff0
Alexandre Cuillerier <alexandre@MacBook-Pro-de-Alexandre.local>
2019-09-27 20:02:42 +0200

    Fix bug

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -15,7 +15,8 @@ class PicturesController < ApplicationController
   end
 
   def edit
-    @picture = Picture.find(1)
+    @picture = Picture.find_by_id(1)
+    @picture = Picture.new if @picture.nil?
   end
 
   def update
