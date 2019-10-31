51264c49f2ef3136fe2877fe6ef6fd4ede3bcb68
Alexandre Cuillerier <alexandre.cuillerier@gmail.com>
2019-10-31 23:27:17 +0100

    Fix bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @current_user = current_user
 
     @news = Article.where(slider: true)
-    @exits_month = Exit.where(date: Date.today.beginning_of_month..Date.today.end_of_month).order("date ASC")
+    @exits_month = Exit.where(date: (Date.today - 20.days)..(Date.today + 20.days)).order("date ASC")
     @picture = Picture.find_by_id(1)
     @stocks = Stock.where(home: true)
   end
